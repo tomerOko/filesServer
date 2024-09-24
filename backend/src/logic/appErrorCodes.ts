8b3c1648fb9dc661d7f23a51a6fc591b69e983b6
tomerOko <tomeroko2@gmail.com>
2024-09-24 18:02:56 +0300

    both sides are running

--- backend/src/logic/appErrorCodes.ts
+++ backend/src/logic/appErrorCodes.ts
@@ -1,6 +1,7 @@
 /* error codes have to be unique in order to find the problem in a fast way with the debugger */
 export const appErrorCodes = {
   REQUEST_VALIDATION_ERROR: 'REQUEST_VALIDATION_ERROR',
+  CONFIG_VALIDATION_ERROR: 'CONFIG_VALIDATION_ERROR',
 } as const;
 
 export type AppErrorCode = (typeof appErrorCodes)[keyof typeof appErrorCodes];
--- backend/src/logic/service.ts
+++ backend/src/logic/service.ts
@@ -1,25 +1,64 @@
+import { File } from 'buffer';
 import { AppError, functionWrapper, getAuthenticatedID } from 'common-lib-tomeroko3';
-import {
-  addTopicRequestType,
-  addTopicResponseType,
-  becomeTeacherRequestType,
-  becomeTeacherResponseType,
-  deleteTopicRequestType,
-  deleteTopicResponseType,
-  stopTeachRequestType,
-  stopTeachResponseType,
-  updateTeacherDetailsRequestType,
-  updateTeacherDetailsResponseType,
-  updateTopicRequestType,
-  updateTopicResponseType,
-} from 'events-tomeroko3';
-
-import {
-  teacherCreatedPublisher,
-  teacherDeactivatedPublisher,
-  teacherUpdatedPublisher,
-  topicCreatedPublisher,
-  topicDeletedPublisher,
-} from '../configs/rabbitMQ/initialization';
+import fs from 'fs';
 
 import { appErrorCodes } from './appErrorCodes';
+
+export const exploreFolders = async (path: string): Promise<string[]> => {
+  return functionWrapper(async () => {
+    const items = fs.readdirSync(path, { withFileTypes: true });
+    const folderNames = items.filter((item) => item.isDirectory()).map((item) => item.name);
+    const fileNames = items.filter((item) => item.isFile()).map((item) => item.name);
+    return [...folderNames, ...fileNames];
+  });
+};
+
+export const createFolder = async (path: string, folderName: string): Promise<void> => {
+  return functionWrapper(async () => {
+    fs.mkdirSync(`${path}/${folderName}`);
+  });
+};
+
+export const renameItem = async (path: string, itemName: string, newName: string): Promise<void> => {
+  return functionWrapper(async () => {
+    const itemPath = `${path}/${itemName}`;
+    const newItemPath = `${path}/${newName}`;
+    fs.renameSync(itemPath, newItemPath);
+  });
+};
+
+export const deleteItem = async (path: string, itemName: string): Promise<void> => {
+  return functionWrapper(async () => {
+    const itemPath = `${path}/${itemName}`;
+    fs.unlinkSync(itemPath);
+  });
+};
+
+export const uploadFile = async (path: string, file: File): Promise<void> => {
+  return functionWrapper(async () => {
+    const filePath = `${path}/${file.name}`;
+    const fileStream = fs.createWriteStream(filePath);
+
+    return new Promise<void>((resolve, reject) => {
+      const reader = file.stream().getReader();
+      const write = async () => {
+        const { done, value } = await reader.read();
+        if (done) {
+          fileStream.end(() => resolve());
+          return;
+        }
+        if (!fileStream.write(value)) {
+          fileStream.once('drain', write);
+        } else {
+          write();
+        }
+      };
+
+      write();
+
+      fileStream.on('error', (error) => {
+        reject(`Error writing file: ${error.message}`);
+      });
+    });
+  });
+};
